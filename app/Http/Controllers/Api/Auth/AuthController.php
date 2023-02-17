586ce7e6b56cf4e3134f5850a43b4b614facbe34
aurora43 <obadahalak538@gmail.com>
2023-02-17 15:03:03 +0200

    upload user image

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -67,7 +67,7 @@ class AuthController extends Controller
             'address' => $request->address,
         ]);
         $return['profile'] = 'user profile updated successfully';
-        if (isset($request->old_password) || !isEmpty($request->old_password)) {
+        if (isset($request->old_password)  && !isEmpty($request->old_password)) {
             if ($this->cheackOldPassword($request->old_password, auth()->user()->password)) {
 
                 $user->update([
@@ -76,6 +76,13 @@ class AuthController extends Controller
 
                 $return['password'] = 'user password changed successfully';
             }
+        }
+        if(isset($request->image) && ! isEmpty($request->image)){
+                $path=$request->image->store('public','userImages');
+                $user->image()->create([
+                    'file'=>'public/'.$path
+                ]);
+                $return['image'] = 'image profile updated successfully';
         }
         return response()->json($return);
     }
