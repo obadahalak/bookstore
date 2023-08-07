ecd18600a4302cd360f9a7135b9ed0a3cb87ee4d
aurora43 <obadahalak538@gmail.com>
2023-08-07 17:41:22 +0300

    create Day rule

---
+++ app/Rules/DayRule.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class DayRule implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $days = ['saterday', 'sunday', 'moday', 'tuesday', 'wednesday', 'tuesday'];
+        $values = collect(array_keys($value));
+        
+        $values->map(function ($value) use ($days, $fail) {
+            if (array_search($value, $days) === false) {
+                $fail("the day $value is not valid");
+            }
+        });
+    
+    }
+}
