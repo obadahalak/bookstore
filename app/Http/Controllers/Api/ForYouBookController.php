4a227adb8c8f895f44af15526b87376465650c36
aurora43 <obadahalak538@gmail.com>
2023-08-11 17:06:36 +0300

    clean up

--- app/Http/Controllers/Api/ForYouBookController.php
+++ app/Http/Controllers/Api/ForYouBookController.php
@@ -1,45 +1,49 @@
 <?php
+
 namespace App\Http\Controllers\Api;
 
 
 use App\Models\Book;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use App\traits\foryouService;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BookResource;
+use App\Models\UserActivity;
 
 class ForYouBookController extends Controller
 {
+    use foryouService;
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-            $books=[];
-           if(auth()->check()){
-         
-            $categories=DB::table('likes')
-            
-                ->where('likes.user_id',auth('user')->id())
-                
-                ->join('books', 'books.id','=','likes.book_id')
-                
-                ->selectRaw('books.category_id, COUNT(likes.id) AS num_likes')
-                
-                ->groupBy('books.category_id')
-                
-                ->orderBy('num_likes','DESC')
-                
-                ->pluck('category_id')
-                
-                ->take(5);
-                
-            }
-          if(!count($categories)){
-            $categories = Category::inRandomOrder()->take(5)->get('id')->pluck('id');
+
+
+        $books = [];
+        if (auth()->check()) {
+
+            $categories = $this->getCategoirsFromWishList();
         }
-            $books=Book::whereIn('category_id',$categories)->paginate(10);
-            return response()->data($books);
-    
+
+        if (!count($categories)) {
+            $categories = Category::inRandomOrder()->take(5)->get('id')->pluck('id')->toArray();
+        }
+
+        $categories = $this->sortingIds($categories);
+       
+        $books = Category::whereIn('id', $categories)
+                ->with(['books' => function ($q) {
+        
+                $q->with(['user', 'category', 'coverImage', 'bookFile', 'likes'])
+        
+                ->latest()->limit(3);
+        
+                }])->get()->pluck('books');
+
+        return  BookResource::collection($books->flatMap(function ($book) {
+            return $book;
+        }));
     }
 }
