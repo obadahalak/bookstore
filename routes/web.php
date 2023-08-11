af45508cc42e7b30a043525346efadee93909498
aurora43 <obadahalak538@gmail.com>
2023-08-11 16:46:33 +0300

    delete web  rotues

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,3 @@ use App\Http\Controllers\WishlistController;
 use App\Http\Controllers\Api\AuthorController;
 use Symfony\Component\Routing\Annotation\Route as test;
 use Laravel\SerializableClosure\Support\ReflectionClosure;
-
-
-Route::get('/download', function () {
-    
-   $book=Link::Where('token',request()->token)->where('active',true)->first();
-    if($book){
-        
-        $book->update([
-            'active'=>false,
-        ]);   
-      return   Storage::disk('public')->download('/books/1.pdf');
-}
-
-
-
-});
