29d768e7eba3c6ce8a96143cdaac4d7084589274
aurora43 <obadahalak538@gmail.com>
2023-08-15 17:24:49 +0300

    add column 'countOfVisits'

---
+++ database/migrations/2023_08_15_131137_add_column_to_user_activities.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_activities', function (Blueprint $table) {
+            $table->bigInteger('count_of_visits')->default(1)->after('book_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_activities', function (Blueprint $table) {
+            $table->dropColumn('count_of_visits');
+        });
+    }
+};
