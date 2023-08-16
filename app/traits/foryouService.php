72d5f68791d2f3614aa2c0e29ae5a695de809fd8
aurora43 <obadahalak538@gmail.com>
2023-08-16 17:46:52 +0300

    update sorting

--- app/traits/foryouService.php
+++ app/traits/foryouService.php
@@ -9,24 +9,24 @@ use Illuminate\Support\Facades\DB;
 trait foryouService{
 
     public function getCategoirsFromWishList(){
-       return  DB::table('likes')
+        return  DB::table('likes')
 
-        ->where('likes.user_id', auth('user')->id())
+            ->where('likes.user_id', auth('user')->id())
 
-        ->join('books', 'books.id', '=', 'likes.book_id')
+            ->join('books', 'books.id', '=', 'likes.book_id')
 
-        ->selectRaw('books.category_id, COUNT(likes.id) AS num_likes')
+            ->selectRaw('books.category_id, COUNT(likes.id) AS num_likes')
 
-        ->groupBy('books.category_id')
+            ->groupBy('books.category_id')
 
-        ->orderBy('num_likes', 'DESC')
+            ->orderBy('num_likes', 'DESC')
 
-        ->pluck('category_id')
+            ->pluck('category_id')
 
-        ->take(5)->toArray();
+            ->take(5)->toArray();
     }
 
     public function sortingIds($categories){
-       return  array_unique(array_merge($categories, UserActivity::getBooksCategories()));
+        return  array_unique(array_merge(UserActivity::getBooksCategories(), $categories));
     }
 }
