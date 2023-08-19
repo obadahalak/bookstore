c543fe2a8eb5b8c16635896a914c492e57524072
aurora43 <obadahalak538@gmail.com>
2023-08-19 18:31:40 +0300

    refactoring and move implementation to ServiceClass

--- app/Http/Controllers/Api/DownloadBookController.php
+++ app/Http/Controllers/Api/DownloadBookController.php
@@ -5,24 +5,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Book;
 use App\Models\Link;
-use App\Events\TrackingUserActivity;
 use Illuminate\Http\Request;
 use App\Http\Services\BookService;
+use App\Events\TrackingUserActivity;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Services\DownloadLinkBookService;
 
 class DownloadBookController extends Controller
 {
-    public function store(Book $book)
+    public function store(Book $book,  DownloadLinkBookService $service)
     {
 
         $token = generate_token();
-
-        $link = Link::create([
-            'token' => $token,
-            'book_id' => $book->id,
-            'url' => $token
-        ]);
+        $link = $service->store($token, $book->id);
         return response()->data($link->url);
     }
 
@@ -30,11 +26,11 @@ class DownloadBookController extends Controller
     public function index()
     {
         $bookLink = Link::whereToken(request()->token)->first();
-       
+
         event(new TrackingUserActivity($bookLink->book_id));
-        if (Storage::disk('public')->exists("/books/$bookLink->book_id.pdf")) 
+        if (Storage::disk('public')->exists("/books/$bookLink->book_id.pdf"))
             return  Storage::disk('public')->download("/books/$bookLink->book_id.pdf");
-        
+
         // BookService::inactivationLink(request()->token); 
         // }
     }
