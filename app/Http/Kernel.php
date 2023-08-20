eac4c9e8ff65acacda03c38d97713e90c7dbbe6a
aurora43 <obadahalak538@gmail.com>
2023-08-20 17:58:24 +0300

    register a new middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,8 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+
+        'is_active_link'=>\App\Http\Middleware\isActiveDownloadLink::class
     ];
    
 }
