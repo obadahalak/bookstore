8ed450c6819cc274775cbbf5704300536721ad5a
aurora43 <obadahalak538@gmail.com>
2023-08-25 10:10:50 +0300

    update DayRule

--- app/Rules/DayRule.php
+++ app/Rules/DayRule.php
@@ -3,6 +3,7 @@
 namespace App\Rules;
 
 use Closure;
+use App\Models\Book;
 use Illuminate\Contracts\Validation\ValidationRule;
 
 class DayRule implements ValidationRule
@@ -12,16 +13,39 @@ class DayRule implements ValidationRule
      *
      * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
      */
+    public $pages = [];
+    public $count_of_selected_pages = 0;
+    public function pages_left()
+    {
+        return Book::getCountPages(request()->book_id) - $this->count_of_selected_pages;
+    }
+    public function isDublicateDate($array, $date)
+    {
+
+        if (collect($array)->where('date', $date)->count() == 1)
+            return true;
+    }
+
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $days = ['saterday', 'sunday', 'moday', 'tuesday', 'wednesday', 'tuesday'];
-       
-        collect($value)->map(function ($value) use ($days, $fail) {
+        $this->count_of_selected_pages = collect($value)->sum('pages');
 
-            if (array_search($value, $days) === false) {
-                $fail("the day $value is not valid");
+        $bookCountPages = Book::getCountPages(request()->book_id);
+
+        collect($value)->each(function ($q) use ($value, $fail) {
+            if ($this->isDublicateDate($value, $q['date']) == false) {
+                $fail('you have dublicated date');
             }
         });
-    
+
+
+
+        if ($this->count_of_selected_pages > $bookCountPages) {
+            $fail('the selected pages are bigger than count of pages book');
+        }
+        if ($this->count_of_selected_pages !== $bookCountPages) {
+            $fail("you stil have {$this->pages_left()} pages, ");
+        }
     }
 }
+
