7611010b7b1a4f9fb4e07581dfce24e6ae4ac08c
aurora43 <obadahalak538@gmail.com>
2023-09-01 15:38:43 +0300

    updated

--- app/Http/Resources/BooksSchedulingResourse.php
+++ app/Http/Resources/BooksSchedulingResourse.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\BooksScheduling;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -15,16 +16,18 @@ class BooksSchedulingResourse extends JsonResource
      */
     public function toArray($request): array
     {
-        $todayName = Carbon::today()->dayName;
-        $status_of_day = isset($this->days->$todayName) ? $this->days->$todayName->status  : 'no task today';
-
+        
+        $startdTask=Carbon::parse($this->started_task);
+        
+        $date=Carbon::parse($this->date);
+        $status_of_day = ($date->format('Y-m-d')==now()->format('Y-m-d')) ? 1  : 0;
 
         return [
             'id' => $this->id,
-            'book_name' => $this->book->name,
-            'pages_must_be_read' => $this->pages_per_day,
-            'started_task' => $this->created_at->longRelativeToNowDiffForHumans(),
-            'duration' => $this->checkDurationTaks($this->created_at->addDays($this->duration)),
+            'book_name' => $this->book_name,
+            'pages_must_be_read' => $this->pages,
+            'started_task' => $startdTask->longRelativeToNowDiffForHumans(),
+            'duration' =>BooksScheduling::checkDurationTask($startdTask->addDays($this->duration)),
             'general_duration' => $this->duration . ' ' . 'days',
             'today' => $status_of_day,
         ];
