d080ed68d4bf176ca3c187eafc2f49e2f428773a
aurora43 <obadahalak538@gmail.com>
2023-09-01 14:06:10 +0300

    init info scheduling

---
+++ app/Models/SchedulingInfo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SchedulingInfo extends Model
+{
+    use HasFactory;
+    protected $table='scheduling_info';
+    protected $guarded=[];
+
+}
